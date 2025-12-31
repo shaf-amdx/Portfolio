45d13dee5d505c2b0380817cd9ff01ff63e57f7b
Shafeeq Ahmed <shafeeqsoft.s@gmail.com>
2025-12-31 12:51:55 +0300

    Update Graph Visualizer.py

--- Basic Graph Visualizer/Graph Visualizer.py
+++ Basic Graph Visualizer/Graph Visualizer.py
@@ -1,66 +1,3 @@
-def draw_graph(func):
-    t.home()
-    for i in range(3000):
-        try:
-            if func=="sinx":
-                y=math.sin(i)*50
-                x=i*5
-            elif func=="cosx":
-                y=math.cos(i)*50
-                x=i*5
-            elif func=="tanx":
-                y=100*(math.sin(i)/math.cos(i))
-                x=i
-            elif func=="cotx":
-                y=100*(math.cos(i)/math.sin(i))
-                x=i
-            elif func=="secx":
-                y=(100/(math.cos(math.radians(i))))
-                x=i
-            elif func=="cosecx":
-                y=(100/(math.sin(math.radians(i))))
-                x=i
-            elif func=="e**x":
-                y=math.exp(i)
-                x=i*100
-            elif "**" in func:
-                y=int(func.split("**")[0])**i
-                x=i*10
-            else:
-                print("Sorry We have not yet developed the program inorder to evaluate ur function (Note: Type 'polynomial' for plotting a polynomial first)!")
-        except ZeroDivisionError:
-            continue
-        t.goto(x,y)
-    s.update()
-def polynomial_graph(y):
-    y=y.replace('^','**')
-    n=0
-    while n<len(y)-1:
-        if y[n].isdigit() and y[n+1].isalpha():
-            y=y[:n+1]+'*'+y[n+1:]
-            n+=1
-        n+=1
-    x=-400
-    t.penup()
-    print(y)
-    while True:
-        y1=eval(y.replace('x',f'({x})'))
-        try:
-            t.goto(x,y1)
-        except:
-            x+=1
-        else:
-            break
-    t.pendown()
-    while x<=400:
-        try:
-            t.goto(x,eval(y.replace('x',f'({x})'))/100)
-        except:
-            pass
-        finally:
-            x+=1
-    s.update()
-func=input("Enter the function you would like to graph (enter 'polynomial' for plotting a polynomial): ").strip().lower()
 import turtle
 import math
 s=turtle.Screen()
@@ -70,8 +7,7 @@ s.bgcolor('black')
 t.pencolor('white')
 t.speed(0)
 t.hideturtle()
-#Drawing the x and y axis
-def draw_axis():
+def draw_axis():#Drawing the x and y axis
     t.forward(1000)
     t.home()
     t.backward(1000)
@@ -81,11 +17,66 @@ def draw_axis():
     t.home()
     t.right(90)
     t.backward(1000)
+def cosec(x):
+    return 1/(math.sin(x))
+def sec(x):
+    return 1/(math.cos(x))
+def cot(x):
+    return 1/(math.tan(x))
+def graph(y):
+    t.home()
+    n=0
+    while n<len(y)-1:
+        if y[n].isdigit() and y[n+1].isalpha():
+            y=y[:n+1]+'*'+y[n+1:]
+            n+=1
+        n+=1
+    y=y.replace('^','**').replace('sin','math.sin').replace('cos','math.cos').replace('tan','math.tan').replace('e**','math.exp').replace('log','math.log')
+    x=-400
+    for i in ['sin','cos','tan','cot','sec','cosec']:
+        if i in y:
+            x_scale=10
+            y_scale=100
+            break
+    else:
+        x_scale=1000
+        y_scale=10
+    t.penup()
+    print(y)
+    while True:
+        try:
+            y1=eval(y.replace('x',f'({x})'))
+            t.goto(x*x_scale,y1*y_scale)
+        except:
+            x+=0.1
+        else:
+            break
+    t.pendown()
+    n=0
+    while n<800:
+        try:
+            min_y,max_y=min(eval(y.replace('x',f'({x})')) for x in range(-400+n,401)),max(eval(y.replace('x',f'({x})')) for x in range(-400,401))
+        except:
+            n+=1
+        else:
+            break
+    y_temp=eval(y.replace('x',f'({x})'))
+    while x<=400:
+        try:
+            y1=eval(y.replace('x',f'({x})'))
+            if abs((y_temp-y1)/(0.1))>=30:
+                t.penup()
+            else:
+                t.pendown()
+            y_temp=y1
+            t.goto(x*10,y1*100)
+        except:
+            pass
+        finally:
+            x+=0.1
+    s.update()
 draw_axis()
 s.update()
-if func=='polynomial':
-    y=input("Enter ur expression in terms of x : ")
-    polynomial_graph(y)
-else:
-    draw_graph(func)
+func=input("Enter the function you would like to graph : ").strip().lower()
+graph(func)
 turtle.done()
